70f3d39f965ea8f3e2617c413ce7c4dc0ea47bd0
chipmash <chipmash@gmail.com>
2017-03-19 12:23:17 +0000

    Initial commit

---
+++ collection/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    # defining the variable
+    number = 6
+    # passing the variable to the view
+    return render(request, 'index.html', {
+        'number': number,
+    })
+
